a923398edea7e1ce2ca0ba8f68429752aa116c90
BelghitiAlaoui-Hicham <belghitiahr@gmail.com>
2021-02-14 20:00:30 +0100

    add scenario

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,18 @@
 import {TennisGame} from "./tennis-game/tennis-game";
+import * as scenario from './scenario.json';
 
-const playerName1 = 'Michel';
-const playerName2 = 'Hicham';
-
+const {playerName1, playerName2, wins} = scenario;
 const tennisGame = new TennisGame(playerName1, playerName2);
-tennisGame.wonPoint(playerName1);
-tennisGame.wonPoint(playerName2);
-tennisGame.wonPoint(playerName2);
-tennisGame.wonPoint(playerName1);
-tennisGame.wonPoint(playerName2);
-tennisGame.wonPoint(playerName1);
-tennisGame.wonPoint(playerName1);
-tennisGame.wonPoint(playerName2);
-tennisGame.wonPoint(playerName2);
-tennisGame.wonPoint(playerName2);
 
 console.log(tennisGame.getScore());
+let c = 0;
+try{
+  do {
+    tennisGame.wonPoint(wins[c]);
+    console.log(tennisGame.getScore());
+    c+=1;
+  }while(wins.length > c && tennisGame.getWinner() === undefined);
+} catch (e) {
+  console.error(e.message);
+}
+
