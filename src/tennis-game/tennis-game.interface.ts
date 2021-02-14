acf042de12b32be661eec6f867c5f8ac68df5a46
BelghitiAlaoui-Hicham <belghitiahr@gmail.com>
2021-02-14 17:59:34 +0100

    add ItennisGame comment

--- src/tennis-game/tennis-game.interface.ts
+++ src/tennis-game/tennis-game.interface.ts
@@ -2,8 +2,31 @@ import {IPlayer} from "../player/player.interface";
 
 export interface ITennisGame {
 
+  /**
+   * Increment point of the player by his name.
+   *
+   * @param playerName - The name input string
+   * @throws player not found, if there is no player with the playerName
+   * @returns void
+   *
+   */
   wonPoint(playerName:string): void;
+
+  /**
+   * returns the Calculated score in the game.
+   *
+   * @returns string
+   *
+   */
   getScore(): string;
+
+  /**
+   * returns the winner of the game.
+   *
+   * @returns {@link IPlayer} if there is a winner.
+   * @returns undefined if there is no winner.
+   *
+   */
   getWinner(): IPlayer | undefined;
 
 }
