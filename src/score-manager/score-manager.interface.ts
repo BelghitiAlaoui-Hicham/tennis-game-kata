e76157c95b57215ccbe68269b65743acfea47463
BelghitiAlaoui-Hicham <belghitiahr@gmail.com>
2021-02-14 17:41:39 +0100

    add comments score manager

--- src/score-manager/score-manager.interface.ts
+++ src/score-manager/score-manager.interface.ts
@@ -1,6 +1,24 @@
 import {IPlayer} from "../player/player.interface";
 
 export interface IScoreManager {
+  /**
+   * returns the Calculated the score in the game.
+   *
+   * @param player1 - The first player {@link IPlayer}
+   * @param player2 - The second player {@link IPlayer}
+   * @returns string
+   *
+   */
   score(player1: IPlayer, player2: IPlayer): string;
+
+  /**
+   * returns the winner of the game.
+   *
+   * @param player1 - The first player {@link IPlayer}
+   * @param player2 - The second player {@link IPlayer}
+   * @returns {@link IPlayer} if there is a winner.
+   * @returns undefined if there is no winner.
+   *
+   */
   playerWinner(player1: IPlayer, player2: IPlayer): IPlayer | undefined;
 }
